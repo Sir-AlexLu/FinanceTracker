733f5858809427dfd631b6c2c74b114a9929673f
Alex Lu <siralexluweb@gmail.com>
2025-11-14 16:17:59 +0530

    Create index.ts

---
+++ backend/src/schemas/index.ts
@@ -0,0 +1,35 @@
+// Authentication schemas
+export * from './auth.schema';
+
+// Account schemas
+export * from './account.schema';
+
+// Transaction schemas
+export * from './transaction.schema';
+
+// Liability schemas
+export * from './liability.schema';
+
+// Bill schemas
+export * from './bill.schema';
+
+// Budget schemas
+export * from './budget.schema';
+
+// Goal schemas
+export * from './goal.schema';
+
+// Settlement schemas
+export * from './settlement.schema';
+
+// Notification schemas
+export * from './notification.schema';
+
+// Voice transaction schemas
+export * from './voiceTransaction.schema';
+
+// Recurring transaction schemas
+export * from './recurringTransaction.schema';
+
+// Common schemas
+export * from './common.schema';
