d1255f9b183d172743f8d8fa04aedb608974583a
Alex Lu <siralexluweb@gmail.com>
2025-11-14 15:04:32 +0530

    Create sensible.ts

---
+++ backend/src/plugins/sensible.ts
@@ -0,0 +1,8 @@
+import fp from 'fastify-plugin';
+import sensible from '@fastify/sensible';
+import { FastifyInstance } from 'fastify';
+
+export default fp(async (fastify: FastifyInstance) => {
+  await fastify.register(sensible);
+  fastify.log.info('✅ Sensible plugin registered');
+});
