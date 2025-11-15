cdf6d69cc1c3ed0f5834384fb531d90c812396f0
Alex Lu <siralexluweb@gmail.com>
2025-11-15 09:36:25 +0530

    Update jwt.ts

--- backend/src/plugins/jwt.ts
+++ backend/src/plugins/jwt.ts
@@ -6,30 +6,18 @@ import { getEnv } from '../config/env';
 export default fp(async (fastify: FastifyInstance) => {
   const env = getEnv();
 
-  // Register JWT plugin with namespace for access tokens
+  // Register JWT plugin for access tokens
   await fastify.register(jwt, {
     secret: env.JWT_ACCESS_SECRET,
     sign: {
       expiresIn: env.JWT_ACCESS_EXPIRES_IN,
     },
-    namespace: 'access',
-    jwtDecode: true,
-  });
-
-  // Register JWT plugin with namespace for refresh tokens
-  await fastify.register(jwt, {
-    secret: env.JWT_REFRESH_SECRET,
-    sign: {
-      expiresIn: env.JWT_REFRESH_EXPIRES_IN,
-    },
-    namespace: 'refresh',
-    jwtDecode: true,
   });
 
   // Decorate with authenticate method
   fastify.decorate('authenticate', async function (request: FastifyRequest, reply: FastifyReply) {
     try {
-      await request.jwtVerify({ namespace: 'access' });
+      await request.jwtVerify();
       // request.user will be automatically populated with userId and email
     } catch (err) {
       reply.status(401).send({
