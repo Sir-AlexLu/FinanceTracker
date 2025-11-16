b4ff1aa0ab30dde00d3987e4425e0c7fbfe643f9
Alex Lu <siralexluweb@gmail.com>
2025-11-16 15:12:02 +0530

    Create ThemeProvider.jsx

---
+++ frontend/components/providers/ThemeProvider.jsx
@@ -0,0 +1,9 @@
+// File: FinanceTracker/frontend/components/providers/ThemeProvider.jsx
+'use client'
+
+import * as React from "react"
+import { ThemeProvider as NextThemesProvider } from "next-themes"
+
+export function ThemeProvider({ children, ...props }) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+}
