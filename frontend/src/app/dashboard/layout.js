947b80bb5c22ca64d6dcdaaebb2f9a087070896d
Alex Lu <siralexluweb@gmail.com>
2025-11-16 21:24:44 +0530

    Update layout.js

--- frontend/src/app/dashboard/layout.js
+++ frontend/src/app/dashboard/layout.js
@@ -3,8 +3,7 @@
 import { useEffect } from 'react'
 import { useRouter } from 'next/navigation'
 import useAuthStore from '@/store/authStore'
-import Navbar from '@/components/Navbar'
-import Sidebar from '@/components/Sidebar'
+import Layout from '@/components/Layout'
 
 export default function DashboardLayout({ children }) {
   const router = useRouter()
@@ -20,15 +19,5 @@ export default function DashboardLayout({ children }) {
     return null
   }
 
-  return (
-    <div className="min-h-screen bg-gray-50">
-      <Navbar />
-      <div className="flex">
-        <Sidebar />
-        <main className="flex-1 p-8">
-          {children}
-        </main>
-      </div>
-    </div>
-  )
+  return <Layout>{children}</Layout>
 }
