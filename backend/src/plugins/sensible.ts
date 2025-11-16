95b4a0d21a031d51430cf4f8c3777324df578ba0
Alex Lu <siralexluweb@gmail.com>
2025-11-16 14:32:49 +0530

    Delete backend directory

--- backend/src/plugins/sensible.ts
+++
@@ -1,7 +0,0 @@
-// src/plugins/sensible.ts
-import fp from 'fastify-plugin';
-import sensible from '@fastify/sensible';
-
-export default fp(async (fastify) => {
-  await fastify.register(sensible);
-});
